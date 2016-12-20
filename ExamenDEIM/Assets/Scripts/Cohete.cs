720bd812beab49938e25c060b32cfda26ecb63e2
Moya1407 <manumoya7@gmail.com>
2016-12-20 20:20:11 +0100

    #13 + #14 Impulsor Nave + Rotación Nave

---
+++ ExamenDEIM/Assets/Scripts/Cohete.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cohete : MonoBehaviour {
+	public float fuerza = 100f;
+	public float giro = 100f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			Debug.Log ("Espacio Pulsado");
+			GetComponent<Rigidbody2D> ().AddForce (transform.up*fuerza);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate(Vector3.forward * giro); 
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate(Vector3.back * giro); 
+		}
+	}
+}
---
+++ ExamenDEIM/Assets/Scripts/Nave.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Nave : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Escenario") {
+			Destroy (gameObject);
+		}
+	}
+}
