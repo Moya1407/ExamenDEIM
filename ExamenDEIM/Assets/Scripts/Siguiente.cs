8b864e4cdb09b6edf5b7315907434931825e45d5
Moya1407 <manumoya7@gmail.com>
2016-12-20 19:46:00 +0100

    #8 Final fútbol

---
+++ ExamenDEIM/Assets/Scripts/Siguiente.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Siguiente : MonoBehaviour {
+	GameObject txt_golesA;
+	Contador ctrl_golesA;
+	GameObject txt_golesR;
+	ContadorR ctrl_golesR;
+
+	// Use this for initialization
+	void Start () {
+		txt_golesA = GameObject.Find ("TextoGolesA");
+		ctrl_golesA = txt_golesA.GetComponent<Contador> ();
+		txt_golesR = GameObject.Find ("TextoGolesR");
+		ctrl_golesR = txt_golesR.GetComponent<ContadorR> ();
+	}
+
+	void Update () {
+			if (ctrl_golesA.golesA == 5) {
+			SceneManager.LoadScene (1);
+		}
+			if (ctrl_golesR.golesR == 5) {
+				SceneManager.LoadScene (1);
+			}
+	}
+}
